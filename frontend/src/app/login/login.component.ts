4daf8f69097f985e01325c4f51350ceaf9473738
marcelafranc <marcelasfranco1@gmail.com>
2024-10-25 20:29:02 -0300

    Sei la nao aguento mais

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+//
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login', // Este será o seu seletor
   standalone: true, // Indica que este é um componente independente
   imports: [
     CommonModule,
+    HttpClientModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    FormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  //providers: [AuthService]
 })
 export class LoginComponent {
-  // Lógica do componente pode ser adicionada aqui
+  email: string = '';
+  senha: string = '';
+
+  onSubmit() {
+    console.log('Tentando fazer login com:', { email: this.email, senha: this.senha });
+
+    };
+  
 }
