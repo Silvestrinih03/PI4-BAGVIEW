f9e40b136999cc4e89813229d7a065a87b37465d
Kenji1655 <kenjigustavo7@hotmail.com>
2024-11-06 06:30:46 -0300

    feat: Alterado página da Home, e alteração na página de cadastro

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { RouterModule } from '@angular/router'; // Importa o RouterModule
-import { routes } from './app.routes'; // Importe as rotas
+import { AppRoutingModule, routes } from './app.routes'; // Importe as rotas
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -17,19 +19,33 @@ import { MenuComponent } from './menu/menu.component';
 import { PagamentoComponent } from './pagamento/pagamento.component';
 import { ConfiguracoesComponent } from './configuracoes/configuracoes.component';
 import { AlugarComponent } from './alugar/alugar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, CadastroComponent, MenuComponent, PagamentoComponent, ConfiguracoesComponent, AlugarComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    MenuComponent,
+    PagamentoComponent,
+    ConfiguracoesComponent,
+    AlugarComponent,
+    HomepageComponent,
+    ErrorpageComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+    MatDatepickerModule,
     MatButtonModule,
+    MatIconModule, // Adicione esta linha
     FormsModule,
-    RouterModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/errorpage/errorpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-errorpage',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './errorpage.component.html',
+  styleUrl: './errorpage.component.css',
+})
+export class ErrorpageComponent {}
