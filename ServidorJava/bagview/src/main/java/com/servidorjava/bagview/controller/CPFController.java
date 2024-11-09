0ab67d2c23dda35e17eae6bce7ed9814a904eb2d
Nicole Silvestrini Garrio <nsgarrio@gmail.com>
2024-11-09 15:54:18 -0300

    Correcao controller CPF

--- ServidorJava/bagview/src/main/java/com/servidorjava/bagview/controller/CPFController.java
+++ ServidorJava/bagview/src/main/java/com/servidorjava/bagview/controller/CPFController.java
@@ -1,13 +1,25 @@
 package com.servidorjava.bagview.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cpf")
+@CrossOrigin(origins = "http://localhost:4201")
 public class CPFController {
 
     @GetMapping("/validar")
     public boolean validarCPF(@RequestParam String cpf) {
+        if (cpf == null || cpf.isEmpty()) {
+            throw new IllegalArgumentException("O CPF não pode ser nulo ou vazio.");
+        }
         return CPFValidacaoTeste.validarCPF(cpf);
     }
 }
+
+// Rodar no postman
+// http://localhost:8080/api/cpf/validar?cpf=12345678909
