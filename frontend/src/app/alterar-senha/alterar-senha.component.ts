8288a688665cd85b325462104c482bf0baf19cc8
Nicole Silvestrini Garrio <nsgarrio@gmail.com>
2024-12-01 15:17:15 -0300

    Alterações front de config

--- frontend/src/app/alterar-senha/alterar-senha.component.ts
+++ frontend/src/app/alterar-senha/alterar-senha.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-alterar-senha',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './alterar-senha.component.html',
   styleUrls: ['./alterar-senha.component.css']
 })
