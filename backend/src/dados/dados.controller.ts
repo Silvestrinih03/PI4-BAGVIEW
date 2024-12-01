9697ad4305da0597dede2a8b2f7d6e6c0d1f4f18
Nicole Silvestrini Garrio <nsgarrio@gmail.com>
2024-12-01 15:01:57 -0300

    back de atualizar senha funcionando

--- backend/src/dados/dados.controller.ts
+++ backend/src/dados/dados.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Patch, Body, Post } from '@nestjs/common';
+import { Controller, Patch, Body, Post, Put, BadRequestException } from '@nestjs/common';
 import { DadosService } from './dados.service';
 import { DadosDto } from './dadosDto';
 
@@ -10,5 +10,20 @@ export class DadosController {
   @Patch('atualizar')
   async atualizarDados(@Body() dadosDto: DadosDto) {
     return this.dadosService.atualizarDados(dadosDto);
+  }
+
+  @Put('alterar-senha')
+  async alterarSenha(@Body() body: { email: string; novaSenha: string }) {
+    const { email, novaSenha } = body;
+
+    if (!email || email.trim() === '') {
+      throw new BadRequestException('O email é obrigatório.');
+    }
+
+    if (!novaSenha || novaSenha.trim() === '') {
+      throw new BadRequestException('A nova senha é obrigatória.');
+    }
+
+    return await this.dadosService.alterarSenha(email, novaSenha);
   }
 }
--- backend/src/dados/dados.service.ts
+++ backend/src/dados/dados.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { DadosDto } from './dadosDto';
 import { UsersService } from '../users/users.service';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class DadosService {
   constructor(private usersService: UsersService) {}
@@ -30,6 +30,37 @@ export class DadosService {
       };
     } catch (error) {
       console.error('Erro ao atualizar dados de pagamento:', error);
+      throw error;
+    }
+  }
+
+  async alterarSenha(email: string, novaSenha: string) {
+    try {
+      if (!email || email.trim() === '') {
+        throw new BadRequestException('O email é obrigatório.');
+      }
+
+      if (!novaSenha || novaSenha.trim() === '') {
+        throw new BadRequestException('A senha não pode estar vazia.');
+      }
+
+      // Criptografar a nova senha
+      //const senhaCriptografada = await bcrypt.hash(novaSenha, 10);
+
+      // Atualizar a senha no banco de dados
+      const usuarioAtualizado = await this.usersService.updateByEmail(email, {
+        password: novaSenha,
+      });
+
+      if (!usuarioAtualizado) {
+        throw new BadRequestException('Usuário não encontrado.');
+      }
+
+      return {
+        message: 'Senha alterada com sucesso.',
+      };
+    } catch (error) {
+      console.error('Erro ao alterar a senha:', error);
       throw error;
     }
   }
